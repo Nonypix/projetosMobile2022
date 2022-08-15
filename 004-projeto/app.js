ab88d9e89c3dd77536a17e6a9c77df8ebb954f9e
Kayke <108700264+Nonypix@users.noreply.github.com>
2022-08-15 20:30:22 -0300

    Add files via upload

---
+++ 004-projeto/app.js
@@ -0,0 +1,16 @@
+var num1 = 5;
+var num2 = 2;
+var num3;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+var num3 = num1/num2;
+console.log(num3)
+var num4 = num1%num2;
+console.log(num4);
+var num3_round = Math.round(num3);
+console.log(num3_round);
+var num5 = 12312214.12498789
+var num5 =num5.toFixed(2);
+console.log(num5);
